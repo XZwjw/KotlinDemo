59bebaae75e3c39cf76a031c9c80e6945c1afeee
wangjiawang <15829348578@163.com>
2018-09-04 11:26:59 +0800

    项目初始化

---
+++ app/src/main/kotlin/com/easy/kotlin/packageDemo.kt
@@ -0,0 +1,20 @@
+package com.easy.kotlin
+
+/**
+ * Description:Kotlin语言基础练习
+ * Created by wangjiawang on 2018/9/4.
+ * complete
+ */
+fun what(){
+    println("This is WHAT ?")
+}
+
+class Motrobike{
+    fun drive() {
+        println("Drive The Motorbike")
+    }
+}
+
+fun main(args:Array<String>) {
+    println("Hello,World!")
+}
